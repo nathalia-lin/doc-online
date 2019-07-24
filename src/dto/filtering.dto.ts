054b5ca2be87d38dd92a8862c73860c9e29fe555
nathalia <nl1532@nyu.edu>
2019-07-24 14:37:50 -0300

    additional input

--- src/dto/filtering.dto.ts
+++
@@ -1,9 +0,0 @@
-export class CreateFilteringDto {
-    readonly networkId?: number;
-    readonly insuranceId?: number;
-    readonly reqDoctorId?: number;
-    readonly consDoctorId?: number;
-    readonly pacientId?: number;
-    readonly date?: Date;
-}
-
